e7a5392a55bb1910260b7d100ddb5e31d35ed4ce
Giovanni Ponciano <ponciano.giovanni15@gmail.com>
2015-01-30 11:29:24 -0800

    pushing3

--- controller/create-db.php
+++ controller/create-db.php
@@ -4,14 +4,33 @@
     $connection = new mysqli($host, $username, $password);
     
     if($connection->connect_error) {
-        die("Error: " .$connection->connect_error);
+        die("<p>Error: " . $connection->connect_error . "</p>");
     }
     
-    $exists = $connectio->select_db($database);
+    $exists = $connection->select_db($database);
     
     if(!$exists) {
-        echo "Database does not exist";
+        $query = $connection->query("CREATE DATABASE $database");
+        
+        if($query) {
+            echo "<p>Succesfully created database:" . $database . "</p>";
+        }
     }
-
+    else {
+        echo "<p>Database already exists.</p>";
+    }
+    
+    $query = $connection->query("CREATE TABLE posts ("
+            . "id int(11) NOT NULL AUTO_INCREMENT,"
+            . "title varchar(255) NOT NULL,"
+            . "post text NOT NULL,"
+            . "PRIMARY KEY (id))");
+    
+    if($query) {
+        echo "<p>Succesfully created table: posts</p>";
+    }
+    else {
+        echo "<p>$connection->error</p>";
+    }
     
     $connection->close();
