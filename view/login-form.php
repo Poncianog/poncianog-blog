16227fc63b5779e2991f351bdc5891d02ff5cb34
Giovanni Ponciano <ponciano.giovanni15@gmail.com>
2015-02-26 10:41:00 -0800

    pushing

---
+++ view/login-form.php
@@ -0,0 +1,21 @@
+<?php
+    require_once(__DIR__ . "/../model/config.php");
+?>
+
+<h1>Login</h1>
+
+<form>
+    <div>
+        <label for="usernane">Username: </label>
+        <input type="text" name="username" />
+    </div>
+    
+    <div>
+        <label for="password">Password: </label>
+        <input type="password" name="password" />
+    </div>
+    
+    <div>
+        <button type="submit">Submit</button>
+    </div>
+</form>
